a593c7e2b2f063e57b9c24aefcde8083bcb38cd2
H. Vetinari <h.vetinari@gmx.com>
2026-01-07 15:31:41 +1100

    clean up left-overs from old CUDA versions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,13 @@
+#!/bin/bash
+set -ex
+
 if [[ "$cuda_compiler_version" == "None" ]]; then
   export FORCE_CUDA=0
 else
   # Borrowed from PyTorch
   # ref: https://github.com/conda-forge/pytorch-cpu-feedstock/blob/7c7a57b7515eaeda67d3879b56b68466f38f0b0d/recipe/build_pytorch.sh#L129-L157
   export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6"
-  if [[ "$cuda_compiler_version" == "11.2" ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;$TORCH_CUDA_ARCH_LIST"
-  elif [[ "$cuda_compiler_version" == "11.8" ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;$TORCH_CUDA_ARCH_LIST;8.9"
-  elif [[ "$cuda_compiler_version" == "12.0" ]]; then
-    export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;8.9;9.0"
-  elif [[ "$cuda_compiler_version" == "12.6" ]]; then
-    export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;8.9;9.0"
-  elif [[ "$cuda_compiler_version" == "12.9" ]]; then
+  if [[ "$cuda_compiler_version" == "12.9" ]]; then
     export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;8.9;9.0;10.0;12.0"
   else
       echo "unsupported cuda version. edit build.sh"
